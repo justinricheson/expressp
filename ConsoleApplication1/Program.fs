38d11fc9d2675ba837fb29874f12b9774c59aaea
Justin Richeson <justinricheson@gmail.com>
2015-05-30 17:20:17 -0500

    Include eof parser

--- ConsoleApplication1/Program.fs
+++ ConsoleApplication1/Program.fs
@@ -9,7 +9,7 @@ open System
 open FParsec
 
 let test p str =
-    match run p str with
+    match run (p .>> eof) str with
     | Success(result, _, _)   -> printfn "Success: %A" result
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
