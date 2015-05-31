27d3ae8c3cf42d4bced28ba37cc6495bed48eec6
Justin Richeson <justinricheson@gmail.com>
2015-05-30 19:15:17 -0500

    Revert to simpler grammar

--- ConsoleApplication1/Program.fs
+++ ConsoleApplication1/Program.fs
@@ -1,41 +1,43 @@
-﻿(*
+﻿open System
+open FParsec
+
+let number = many1 digit |>> fun ds -> float <| String.Concat(ds)
+
+let op : Parser<float -> float -> float, unit> =
+    charReturn '+' (+) <|>
+    charReturn '-' (-) <|>
+    charReturn '*' (*) <|>
+    charReturn '/' (/)
+
+let expression, expressionImpl = createParserForwardedToRef()
+do expressionImpl :=
+    choice[
+        attempt(pipe3 number op expression (fun x y z -> y x z));
+        number]
+
+let test p str =
+    match run (p .>> eof) str with
+    | Success(result, _, _) -> printfn "Success: %A" result
+    | Failure(result, _, _) -> printfn "Failure: %A" result
+
+[<EntryPoint>]
+let main argv = 
+
+    test expression "1+36/3*4-2"
+    Console.Read() |> ignore
+
+    0
+
+(*
+    Simplified:
+    expression: number | number op expresion
+    op: +, -, *, /
+    number: digit*
+    digit: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+    Complete:
     expression: number | (expression) | expression op expresion
     op: +, -, *, /
     number: digit*
     digit: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 *)
-
-open System
-open FParsec
-
-let test p str =
-    match run (p .>> eof) str with
-    | Success(result, _, _)   -> printfn "Success: %A" result
-    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
-
-let number = many1 digit |>> fun ds -> int <| String.Concat(ds)
-
-let op : Parser<int -> int -> int, unit> =
-    charReturn '+' (+) <|>
-    charReturn '-' (-) <|>
-    charReturn '*' (*) <|>
-    charReturn '/' (/)
-    
-let expression, expressionImpl = createParserForwardedToRef()
-do expressionImpl :=
-    choice[
-        number;
-        between (pchar '(') (pchar ')') expression;]
-    
-
-[<EntryPoint>]
-let main argv = 
-
-    test expression "((123123))"
-    Console.Read()
-
-    0
-
-// paren expression paren
-// number op
-// single number
