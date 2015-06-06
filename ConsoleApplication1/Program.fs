5ca6ae8ecc4d53157e1fca367a3fee8733952ab6
Justin Richeson <justinricheson@gmail.com>
2015-06-06 11:43:59 -0500

    Start implementing parens

--- ConsoleApplication1/Program.fs
+++ ConsoleApplication1/Program.fs
@@ -6,37 +6,46 @@ let rec evaluate x y =
     | [] -> x
     | (a, b)::t -> evaluate (a x b) t
 
-let number : Parser<_, unit> =
-    spaces
-    >>. numberLiteral NumberLiteralOptions.AllowMinusSign "Invalid number" .>>
-    spaces
-    |>> fun num -> float num.String
+let betweenSpaces p = between spaces spaces p
 
-let op : Parser<_, unit> =
+let number =
+    betweenSpaces <| numberLiteral
+        NumberLiteralOptions.AllowMinusSign
+        "Invalid number"
+        |>> fun num -> float num.String
+
+let op =
     charReturn '+' (+) <|>
     charReturn '-' (-) <|>
     charReturn '*' (*) <|>
     charReturn '/' (/)
 
-let opNum : Parser<_, unit> =
-    pipe2 op number (fun x y -> (x, y))
+let opNum = pipe2 op number (fun x y -> (x, y))
 
-let expression : Parser<_, unit> =
-    attempt(pipe2 number (many1 opNum) (fun x y -> evaluate x y))
+let opString =
+    attempt(pipe2 number (many1 opNum)
+        (fun x y -> evaluate x y))
     <|> number
 
+let lParen : Parser<_, unit> = betweenSpaces <| skipChar '('
+let rParen : Parser<_, unit> = betweenSpaces <| skipChar ')'
+
+let expression =
+    attempt(opString .>> eof) <|>
+    attempt(between lParen rParen opString)
+
 let test p str =
-    match run (p .>> eof) str with
+    match run p str with
     | Success(result, _, _) -> printfn "Success: %A" result
     | Failure(result, _, _) -> printfn "Failure: %A" result
 
 [<EntryPoint>]
 let main argv = 
-    test expression "    1\t\t"
+    test expression "  (  1\t)\t"
     test expression "1 +
     2"
     test expression "1+2-3"
-    test expression "-3+1"
+    test expression " -3+1"
     test expression "1+36/3*4-2"
     Console.Read() |> ignore
 
