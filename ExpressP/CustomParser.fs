75ba4da69d8625aea1920707c0efb815b97503d1
Justin Richeson <justinricheson@gmail.com>
2015-06-13 12:57:42 -0500

    Implement Or

--- ExpressP/CustomParser.fs
+++ ExpressP/CustomParser.fs
@@ -7,11 +7,17 @@ type ParseResult<'a> =
     }
 
 let Fail = fun input -> None
-let Return a = fun input -> { Result = a; Rest = input}
+let Return a = fun input -> { Result = Some a; Rest = input}
 let Then p f =
     fun input ->
-        let r = p(input)
+        let r = p input
         match r with
         | None -> None
-        | _ -> f(r.Value.Result)(r.Value.Rest)
+        | _ -> (f r.Value.Result) r.Value.Rest
 let Then_ p1 p2 = fun () -> Then p1 (fun r -> p2)
+let Or p1 p2 =
+    fun input ->
+        let r = p1 input
+        match r with
+        | None -> p2 input
+        | _ -> r
