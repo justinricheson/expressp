dc357a239d12e0b773e61d81146d2e5a18068bcd
Justin Richeson <justinricheson@gmail.com>
2015-06-13 14:51:24 -0500

    Fix some type issues

--- ExpressP/CustomParser.fs
+++ ExpressP/CustomParser.fs
@@ -2,22 +2,32 @@
 
 type ParseResult<'a> =
     {
-        Result : 'a;
+        Result : Option<'a>;
         Rest : string
     }
 
-let Fail = fun input -> None
-let Return a = fun input -> { Result = Some a; Rest = input}
-let Then p f =
+type Parser<'a> = string -> ParseResult<'a>
+
+let Fail     : Parser<_> = fun input -> { Result = None;   Rest = input }
+let Return a : Parser<_> = fun input -> { Result = Some a; Rest = input }
+
+let ThenBind p (f : Option<_> -> Parser<'b>) : Parser<'b> =
     fun input ->
         let r = p input
-        match r with
-        | None -> None
-        | _ -> (f r.Value.Result) r.Value.Rest
-let Then_ p1 p2 = fun () -> Then p1 (fun r -> p2)
-let Or p1 p2 =
+        match r.Result with
+        | None -> { Result = None; Rest = input } // Have to recreate the result since p returns a Parser<'a>
+        | _ -> (f r.Result) r.Rest
+let Then p1 p2 : Parser<_> = ThenBind p1 (fun r -> p2)
+
+let Or p1 p2 : Parser<_> =
     fun input ->
         let r = p1 input
-        match r with
+        match r.Result with
         | None -> p2 input
         | _ -> r
+
+// Then op (fun f -> Then p (fun y -> ChainlHelper (fun () -> f x y) p op))
+let ChainlHelper a p op =
+    Or
+        <| (Then op op)
+        <| Return a
