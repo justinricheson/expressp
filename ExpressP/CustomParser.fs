f48f1609803f81bf0f7cca60cfd1fd544e643df7
Justin Richeson <justinricheson@gmail.com>
2015-06-28 13:27:39 -0500

    Implement Chainr1

--- ExpressP/CustomParser.fs
+++ ExpressP/CustomParser.fs
@@ -6,27 +6,25 @@ type ParseResult<'a> =
         Rest : string
     }
 
-type Parser<'a> = string -> ParseResult<'a>
+let Fail     = fun input -> { Result = None;   Rest = input }
+let Return a = fun input -> { Result = Some a; Rest = input }
 
-let Fail     : Parser<_> = fun input -> { Result = None;   Rest = input }
-let Return a : Parser<_> = fun input -> { Result = Some a; Rest = input }
-
-let ThenBind p f : Parser<'b> =
+let ThenBind p f =
     fun input ->
         let r = p input
         match r.Result with
         | None -> { Result = None; Rest = input } // Have to recreate the result since p returns a Parser<'a>
         | _ -> (f r.Result) r.Rest
-let Then p1 p2 : Parser<_> = ThenBind p1 (fun r -> p2)
+let Then p1 p2 = ThenBind p1 (fun r -> p2)
 
-let Or p1 p2 : Parser<_> =
+let Or p1 p2 =
     fun input ->
         let r = p1 input
         match r.Result with
         | None -> p2 input
         | _ -> r
 
-let rec Chainl1Helper a p (op : Parser<'a -> 'a -> 'a>) : Parser<_> =
+let rec Chainl1Helper a p op =
     Or
         <| ThenBind  op (fun f ->
            ThenBind   p (fun y ->
@@ -34,7 +32,15 @@ let rec Chainl1Helper a p (op : Parser<'a -> 'a -> 'a>) : Parser<_> =
         <| Return a
 let Chainl1 p op = ThenBind p (fun x -> Chainl1Helper x.Value p op)
 
-// TODO
-let Chainr1 = true
+let rec Chainr1 p op =
+    ThenBind p (fun x ->
+        Or
+            (ThenBind op (fun f ->
+                ThenBind (Chainr1 p op) (fun y ->
+                    Return (f.Value x.Value y.Value))))
+            (Return x.Value))
+
+
+
 let Nat = true
 let Literal = true
